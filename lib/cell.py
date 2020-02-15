1e0e0633db8ec917f004cfce912c3f87a7d89b87
Tyler Bierwirth <tylerbierwirth@gmail.com>
2020-02-15 14:55:50 -0700

    Add isEmpty function to cell class

--- lib/cell.py
+++ lib/cell.py
@@ -2,3 +2,9 @@ class Cell(object):
     def __init__(self, coordinate):
         self.coordinate = coordinate
         self.ship = None
+
+    def isEmpty(self):
+        if self.ship == None:
+            return True
+        elif self.ship != None:
+            return False
