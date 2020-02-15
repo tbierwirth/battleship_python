ca6eea60408ffd5eb86023d9f029f6c132f207f0
Tyler Bierwirth <tylerbierwirth@gmail.com>
2020-02-15 14:26:31 -0700

    Add is_sunk function to ship class

--- lib/ship.py
+++ lib/ship.py
@@ -2,3 +2,10 @@ class Ship(object):
     def __init__(self, name, length):
         self.name = name
         self.length = length
+        self.health = length
+
+    def is_sunk(self):
+        if self.health > 0:
+            return False
+        elif self.heatlh <= 0:
+            return True
