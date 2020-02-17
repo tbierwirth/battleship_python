a96ff44d6400726f1dc28c267f80f47dee4c7474
Tyler Bierwirth <tylerbierwirth@gmail.com>
2020-02-17 15:24:47 -0700

    Add isValidPlacement function to board class to check that the given coordinates are valid and not diagonal

--- lib/board.py
+++ lib/board.py
@@ -15,3 +15,31 @@ class Board(object):
             return False
         else:
             return True
+
+    def isValidPlacement(self, ship, coordinates):
+        cells = list(self.cells)
+        letters = []
+        numbers = []
+        for coordinate in coordinates:
+            letter, number = coordinate
+            letters.append(letter)
+            numbers.append(number)
+        if ship.length == len(coordinates) and (len(set(numbers)) != len(set(letters))):
+            if len(set(letters)) == 1 and len(set(numbers)) != 1:
+                consecutive = []
+                for i in range(len(numbers) - 1):
+                    consecutive.append(int(numbers[i+1]) - int(numbers[i]) == 1)
+                if all(consecutive):
+                    return True
+                else:
+                    return False
+            elif len(set(numbers)) == 1 and len(set(letters)) != 1:
+                consecutive = []
+                for i in range(len(letters) - 1):
+                    consecutive.append(ord(letters[i+1]) - ord(letters[i]) == 1)
+                if all(consecutive):
+                    return True
+                else:
+                    return False
+        else:
+            return False
