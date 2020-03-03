dd7e54f7fffcfd07166ea2126f27745e6d4df605
Tyler Bierwirth <tylerbierwirth@gmail.com>
2020-03-03 16:28:54 -0700

    Add feature to choose coordinates for player ships that checks if placement is valid

--- battleship.py
+++ battleship.py
@@ -25,6 +25,37 @@ if choice.lower() == "p":
             break
     player = Board(int(width), int(height))
     cpu = Board(int(width), int(height))
+
+    os.system('clear')
+
+    pc_submarine = Ship("Submarine", 2)
+    pc_cruiser = Ship("Cruiser", 3)
+    npc_submarine = Ship("Submarine", 2)
+    npc_cruiser = Ship("Cruiser", 3)
+
+    while True:
+        os.system('clear')
+        print(player.render(True))
+        sub_coords = input("Pick two empty coordinates for your submarine, space separated ('A1 A2') \n")
+        if player.isValidPlacement(pc_submarine, sub_coords.split(' ')) == False:
+            os.system('clear')
+            print(player.render(True))
+            print("Please pick empty coordinates that are consecutive and not diagonal")
+        else:
+            player.place(pc_submarine, sub_coords.split(' '))
+            break
+
+    while True:
+        os.system('clear')
+        print(player.render(True))
+        cruiser_coords = input("Pick three empty coordinates, space separated ('A1 A2 A3') \n")
+        if player.isValidPlacement(pc_cruiser, cruiser_coords.split(' ')) == False:
+            os.system('clear')
+            print(player.render(True))
+            print("Please pick empty coordinates that are consecutive and not diagonal")
+        else:
+            player.place(pc_cruiser, cruiser_coords.split(' '))
+            break
 elif choice.lower() == "q":
     print("Quit")
 else:
